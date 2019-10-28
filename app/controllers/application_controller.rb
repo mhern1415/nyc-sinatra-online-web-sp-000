c30612cc5e297c9e96995c95c85712adca30e4f1
Mark Hernandez <m.hern1415@gmail.com>
2019-10-28 19:10:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < Sinatra::Base
-  set :views, proc { File.join(root, '../views/') }
+
+  set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
 
   get '/' do
-    erb :"application/index"
+    erb :index
   end
 end
